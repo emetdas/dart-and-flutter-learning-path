df76537f71834f958dac33433ed3c9eee833afae
emetdas <72676990+emetdas@users.noreply.github.com>
2020-12-07 19:41:18 +0600

    new dart project

--- main.dart
+++ main.dart
@@ -1,59 +1,124 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home()
-      ,
+      home: Homepage(),
     ));
 
-class Home extends StatelessWidget {
+class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return
-     Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: Text('Emet Web Hub'),
-        elevation: 0,
+        title: Text("Food Daliy Bari App"),
         centerTitle: true,
-        backgroundColor: Colors.purpleAccent[800],
+        elevation: 0.0,
+        backgroundColor: Colors.deepOrange,
       ),
-      body: 
-      Column(children: <Widget>[
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 10.0,vertical: 20),
-          child: Text('Bangladesh',
-          style: TextStyle(fontSize: 20.0,color: Colors.purple),          
-          ),
-        ),
-        Container(
-          margin: EdgeInsets.fromLTRB(20, 30, 20, 0),
-          padding: EdgeInsets.all(20),
-          decoration: BoxDecoration(
-            color: const Color(0xFF370A8B),
-            border: 
-            Border.all(width: 1.0),
-          ),
-          child: Text("Use global keys to uniquely identify child widgets. Global keys must be keys to uniquely keys to uniquelykeys to uniquelykeys to uniquely",
-          style: TextStyle(fontFamily: 'Ubuntu',color: Colors.white,fontSize: 16),
-          )
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-           Container(
-             margin: EdgeInsets.symmetric(horizontal: 10,vertical: 30.0),
-             width: 300,
-             height: 300,
-             decoration: BoxDecoration(
-               color: Color(0xFF8B0A3B),
-             ),
-           )
+      body:
+       Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              padding: EdgeInsets.all(20),
+              color: Colors.pinkAccent,
+              child: Icon(Icons.card_travel, color: Colors.white),
+            ),
+            Container(
+              padding: EdgeInsets.all(20),
+              color: Colors.orangeAccent,
+              child: Icon(Icons.food_bank, color: Colors.white),
+            ),
+            Container(
+              padding: EdgeInsets.all(20),
+              color: Colors.orange[900],
+              child: Icon(Icons.agriculture, color: Colors.white),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              width: 400,
+              height: 300,
+              child: 
+              Image.network("https://images.unsplash.com/photo-1593642634524-b40b5baae6bb?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80",
+              fit: BoxFit.cover,
+              )
+              ,
+            ),
           ],
-        )
-      ],) 
-      ,
-     );
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('floating action button press');
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.pink,
+        elevation: 0.7,
+      ),
+      drawer: Drawer(
+      child: Container(
+        padding: EdgeInsets.all(5.0),
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                      "https://images.unsplash.com/photo-1593642634524-b40b5baae6bb?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              accountName: Text("Emet Das"),
+              accountEmail: Text("emetdas552@gmail.com"),
+              currentAccountPicture: GestureDetector(
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"),
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text("profile",
+              style: TextStyle(
+                color: Colors.white,
+              ), ),
+              trailing: Icon(Icons.account_box,color: Colors.white,),
+              onTap: () {
+                print("icon Tap");
+              },
+              tileColor: Colors.purple[500],
+            ),
+            Divider(height: 10.0,),
+            ListTile(
+              title: Text("Gallary",
+              style: TextStyle(
+                color: Colors.white,
+              ),              
+              ),
+              trailing: Icon(Icons.add_a_photo_outlined,color: Colors.white,),
+              tileColor: Colors.pink[400],
+              onTap: () {
+                print("icon Tap");
+              },
+            ),
+            Divider(height: 10.0,),
+            ListTile(
+              title: Text("Map",
+              style: TextStyle(
+                color: Colors.white
+              ),),
+              trailing: Icon(Icons.map_outlined,color: Colors.white,),
+              onTap: () {
+                print("icon Tap");
+              },
+              tileColor: Colors.red[500],
+            ),
+          ],
+        ),
+        ),
+      ),
+    );
   }
 }
